2ff2b7fb90aed22dad9d6100364950ffb0f5392d
YilinH <e502122008@gmail.com>
2021-12-17 10:36:01 +0800

    建立branch延遲載入路由模組

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'branch', loadChildren: () => import('./branch/branch.module').then(m => m.BranchModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lazy-load-demo';
+  title = 'Lazy-Load-Demo';
 }
